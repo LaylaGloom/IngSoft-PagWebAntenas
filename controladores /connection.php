dc75863e92c3d244663b8041208acf8fc1fd8399
WichoChuletas <38122088+WichoChuletas@users.noreply.github.com>
2018-04-15 22:09:50 -0500

    crear connection.php
    connexion con la base de datos

---
+++ controladores /connection.php
@@ -0,0 +1,13 @@
+<?php 
+$server = "127.0.0.1";
+$user = "root";
+$password = "coffee";
+$bd = "Antena";
+try {
+	$mysqli = new mysqli($server, $user, $password, $bd);
+} catch (mysqli_sql_exception $e) {
+    throw $e;
+    echo "Error: "+$e;
+} 
+?>
+

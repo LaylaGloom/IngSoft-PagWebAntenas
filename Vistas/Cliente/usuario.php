b35653b5033d520255f8d032ba8ba5de34033d31
WichoChuletas <38122088+WichoChuletas@users.noreply.github.com>
2018-04-19 17:45:16 -0500

    conexion de vistas con controladores
    login
    logout
    registro

---
+++ Vistas/Cliente/usuario.php
@@ -0,0 +1,14 @@
+ <?php include ('head.php'); ?>
+ <?php include ('menu.php'); ?>
+ <?php include ('carrito.php'); ?>
+ <div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-md-9 col-sm-12 form-reg">
+      <div class="f-cafe">
+   		<a href="../../controladores/logout.php">Cerrar Sesión</a>
+      </div>
+    </div>
+  </div>
+</div>
+ <?php include ('body.php'); ?>
+ <?php include ('footer.php'); ?>

c7a3a943521ae54f63b844dd0319db4edf37391a
WichoChuletas <38122088+WichoChuletas@users.noreply.github.com>
2018-04-19 23:18:52 -0500

    controladores_login_logout_register
    revisar funcionamiento y redireccionamiento

--- controladores/login.php
+++ controladores/login.php
@@ -1,35 +1,18 @@
-<?php
+<?php 
+include ('connection.php'); 
+include ('funciones.php');
+if (!isset($_SESSION)){
 session_start();
-include ('connection.php');
+$errors = array();
 if(isset($_POST['ingresar'])){
-  if ((isset($_POST['correo'])) && (isset($_POST['contraseña']))) {
-    $email = $_POST['correo'];
-    $passw = $_POST['contraseña'];
-    $contraseña_hash = hash('sha256', $passw);
-    try{
-      $query = "SELECT * FROM clientes WHERE email ='".$email."' AND contraseña = '".$contraseña_hash."';";
-      $execute = $mysqli->query($query);
-      $row = $execute->fetch_array();
-    }
-    catch(mysqli_sql_exception $e){
-      throw $e;
-    }
-  }else{
-      echo "rellena los campos";
+  
+  $email = $mysqli->real_escape_string($_POST['correo']);
+  $passw = $mysqli->real_escape_string($_POST['contraseña']);
+  if(isNullLogin($email, $passw)){
+    $errors[]="Rellene todos los campos";
   }
-
-  if(($email == $row['email']) && ($contraseña_hash == $row['contraseña'])){
-    $_SESSION['username']=$row['nombres'];
-    header("location: ../Vistas/Cliente/home.php");
-  }
-  else{
-    header("location: ../Vistas/Cliente/login.php");
-    echo "<p>El correo electrónico o la contraseña son incorrectos</p>";
-
-  }
-}
-else{
-  echo "rellena los campos";
-	header("location: ../Vistas/Cliente/login.php");
+  $errors[]=login($email, $passw);  
+ }
 }
 ?>
+
